d7c3c07ce566b41628e6f237996a60e51d217350
Ethel Cofresi <cofresi.ethel@gmail.com>
2019-02-27 13:19:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+<<<<<<< HEAD
   get '/posts/new' do 
     
     erb :new
@@ -49,5 +50,46 @@ class ApplicationController < Sinatra::Base
     @post = Post.find(params[:id])
     @post.delete
     erb :delete
+=======
+  get '/posts/new' do #loads new form
+    erb :new
   end
+
+  post '/posts' do  #creates a post
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+  get '/posts' do #loads index page
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do  #loads show page
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do #loads edit form
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+>>>>>>> f6be36ed8ace48035353d6ad663efb02d3c8fda3
+  end
+
+  patch '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
+
 end
